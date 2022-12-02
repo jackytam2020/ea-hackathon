fa7467a5384cd278dfa1ecad172411954d121e04
wilsonn-g <wilson-gee@hotmail.com>
2022-12-02 12:49:52 -0800

    flickering

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,16 @@ import "./Header.scss";
 
 import AccessibilityIcon from "../../assets/images/AssistiveTechlogo.png";
 
+import { useInView } from "react-intersection-observer";
+
 export default function Header() {
+  const { ref: header, inView: isHeaderVisible } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
+
   return (
-    <section className="header">
+    <section ref={header} className={`header ${isHeaderVisible ? "" : "header--hidden"}`}>
       <button className="header__button">Sign In</button>
       <button className="header__button">Join Now</button>
       <img src={AccessibilityIcon} alt="handicap sign and laptop icon" className="header__accessibility-icon"></img>
--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,9 +1,19 @@
 import "./NavBar.scss";
 
 import Logo from "../../assets/logos/EALogo.png";
+
+import { useInView } from "react-intersection-observer";
+
 export default function NavBar() {
+  const { ref: nav, inView: isNavVisible } = useInView({
+    /* Optional options */
+    trackVisibility: true,
+    initialInView: true,
+    delay: 100,
+    threshold: 1,
+  });
   return (
-    <nav className="nav">
+    <nav ref={nav} className={`nav ${isNavVisible ? "" : "nav--fixed"}`}>
       <img className="nav__logo" src={Logo} alt="Electronic Arts Logo"></img>
       <div className="nav__ctas">
         <button className="nav__button">CTA</button>
