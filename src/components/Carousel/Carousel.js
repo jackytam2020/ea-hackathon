0a8d3af492c4f722da38d0fe5e436cdcab6c920a
wilsonn-g <wilson-gee@hotmail.com>
2022-12-02 05:07:41 -0800

    added carousel images and gradient buttons/background

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,23 +1,27 @@
-import React, { useRef } from 'react';
-import './Carousel.scss';
-import { useDraggable } from 'react-use-draggable-scroll';
+import React, { useRef } from "react";
+import "./Carousel.scss";
+import { useDraggable } from "react-use-draggable-scroll";
+
+// assets
+import ApexLegends from "../../assets/images/apexlegends.jpg";
+import Deadspace from "../../assets/images/deadspace.jpg";
+import Fifa from "../../assets/images/fifa.png";
+import Madden from "../../assets/images/madden-nfl-23.jpg";
+import NeedForSpeed from "../../assets/images/nfsunbound.jpg";
+import Sims from "../../assets/images/sims.png";
 
 function Carousel(props) {
   const ref = useRef();
   const { events } = useDraggable(ref);
 
+  const carouselImgs = [ApexLegends, Deadspace, Fifa, Madden, NeedForSpeed, Sims];
+
   return (
     <div className="carousel-container">
       <div className="carousel" {...events} ref={ref}>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
-        <div className="carousel__item"></div>
+        {carouselImgs.map((game) => {
+          return <img className="carousel__item" src={game} alt="electronic arts game"></img>;
+        })}
       </div>
     </div>
   );
