af02b729e2458acec47c137b77429e6d0721e74f
Priscila M. Moura <priscila.moura@gmail.com>
2022-12-02 01:05:03 -0800

    created markup for graphics section

---
+++ src/components/graphicsSection/GraphicsSection.js
@@ -0,0 +1,24 @@
+import "./GraphicsSection.scss";
+
+export default function GraphicsSection() {
+  return (
+    <section className="graphics">
+      <div className="graphics__container">
+        <h2 className="graphics__section-header">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        </h2>
+        <div className="graphics__image-text-wrapper">
+          <div className="graphics__image-container">
+            <img className="graphics__image" src="" alt="" />
+          </div>
+          <p className="graphics__text">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            ipsam amet excepturi natus odio voluptatum porro itaque veniam earum
+            vitae ex aut quibusdam, debitis quas nesciunt tempora minus rem
+            fugit?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
