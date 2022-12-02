fd948a90583b68726c64244e1bbcc6cff4ceac6c
Priscila M. Moura <priscila.moura@gmail.com>
2022-12-02 12:48:04 -0800

    finished nav bar

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -1,13 +1,35 @@
 import "./NavBar.scss";
-
+import chevron from "../../assets/images/arrow_drop_down-24px.svg";
 import Logo from "../../assets/logos/EALogo.png";
 export default function NavBar() {
   return (
     <nav className="nav">
-      <img className="nav__logo" src={Logo} alt="Electronic Arts Logo"></img>
       <div className="nav__ctas">
-        <button className="nav__button">CTA</button>
-        <button className="nav__button">CTA</button>
+        <button className="nav__button">Sing In</button>
+        <button className="nav__button">Join Now</button>
+      </div>
+      <div className="nav__drop-down-container">
+        <img className="nav__logo" src={Logo} alt="Electronic Arts Logo" />
+        <span className="nav__text">
+          Games
+          <img className="nav__icon" src={chevron} alt="drop down chevron" />
+        </span>
+        <span className="nav__text">
+          More Experiences
+          <img className="nav__icon" src={chevron} alt="drop down chevron" />
+        </span>
+        <span className="nav__text">
+          About
+          <img className="nav__icon" src={chevron} alt="drop down chevron" />
+        </span>
+        <span className="nav__text">
+          Commitment
+          <img className="nav__icon" src={chevron} alt="drop down chevron" />
+        </span>
+        <span className="nav__text">
+          Resources
+          <img className="nav__icon" src={chevron} alt="drop down chevron" />
+        </span>
       </div>
     </nav>
   );
