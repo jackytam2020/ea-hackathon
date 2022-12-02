29e8f3be580d9cbc06cd35442be6b215a22f1a3f
unknown <jackytam2020@gmail.com>
2022-12-02 12:08:00 -0800

    final changes to carousel

---
+++ src/components/VideoModal/VideoModal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './VideoModal.scss';
+import Modal from 'react-modal';
+
+function VideoModal(props) {
+  return (
+    <div className="video-modal">
+      <Modal
+        isOpen={props.modalIsOpen}
+        ariaHideApp={false}
+        className="video-modal__modal-container"
+        overlayClassName="video-modal__overlay"
+      >
+        <iframe width="100%" height="400px" src={props.videoSrc}></iframe>
+      </Modal>
+    </div>
+  );
+}
+
+export default VideoModal;
