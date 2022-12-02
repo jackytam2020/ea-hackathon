46e4e3848ac61ad74cb099c1be817281d155c8b4
wilsonn-g <wilson-gee@hotmail.com>
2022-12-02 13:14:45 -0800

    hacky navbar

--- src/components/nav-bar/NavBar.js
+++ src/components/nav-bar/NavBar.js
@@ -4,6 +4,10 @@ import Logo from "../../assets/logos/EALogo.png";
 
 import { useInView } from "react-intersection-observer";
 
+import "./Header.scss";
+
+import AccessibilityIcon from "../../assets/images/AssistiveTechlogo.png";
+
 export default function NavBar() {
   const { ref: nav, inView: isNavVisible } = useInView({
     /* Optional options */
@@ -12,36 +16,43 @@ export default function NavBar() {
     delay: 100,
     threshold: 1,
   });
+  const { ref: header, inView: isHeaderVisible } = useInView({
+    /* Optional options */
+    threshold: 0.5,
+  });
+
   return (
-    <nav ref={nav} className={`nav ${isNavVisible ? "" : "nav--fixed"}`}>
-      <img className="nav__logo" src={Logo} alt="Electronic Arts Logo"></img>
-      <div className="nav__ctas">
-        <button className="nav__button">Sing In</button>
-        <button className="nav__button">Join Now</button>
-      </div>
-      <div className="nav__drop-down-container">
-        <img className="nav__logo" src={Logo} alt="Electronic Arts Logo" />
-        <span className="nav__text">
-          Games
-          <img className="nav__icon" src={chevron} alt="drop down chevron" />
-        </span>
-        <span className="nav__text">
-          More Experiences
-          <img className="nav__icon" src={chevron} alt="drop down chevron" />
-        </span>
-        <span className="nav__text">
-          About
-          <img className="nav__icon" src={chevron} alt="drop down chevron" />
-        </span>
-        <span className="nav__text">
-          Commitment
-          <img className="nav__icon" src={chevron} alt="drop down chevron" />
-        </span>
-        <span className="nav__text">
-          Resources
-          <img className="nav__icon" src={chevron} alt="drop down chevron" />
-        </span>
-      </div>
-    </nav>
+    <>
+      <section ref={header} className={`header ${isHeaderVisible ? "" : "header--hidden"}`}>
+        <button className="header__button">Sign In</button>
+        <button className="header__button">Join Now</button>
+        <img src={AccessibilityIcon} alt="handicap sign and laptop icon" className="header__accessibility-icon"></img>
+      </section>
+      <nav ref={nav} className={`nav ${isHeaderVisible ? "" : "nav--fixed"}`}>
+        <div className="nav__drop-down-container">
+          <img className="nav__logo" src={Logo} alt="Electronic Arts Logo" />
+          <span className="nav__text">
+            Games
+            <img className="nav__icon" src={chevron} alt="drop down chevron" />
+          </span>
+          <span className="nav__text">
+            More Experiences
+            <img className="nav__icon" src={chevron} alt="drop down chevron" />
+          </span>
+          <span className="nav__text">
+            About
+            <img className="nav__icon" src={chevron} alt="drop down chevron" />
+          </span>
+          <span className="nav__text">
+            Commitment
+            <img className="nav__icon" src={chevron} alt="drop down chevron" />
+          </span>
+          <span className="nav__text">
+            Resources
+            <img className="nav__icon" src={chevron} alt="drop down chevron" />
+          </span>
+        </div>
+      </nav>
+    </>
   );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './HomePage.scss';
+import React from "react";
+import "./HomePage.scss";
 
-import NavBar from '../components/nav-bar/NavBar';
-import Carousel from '../components/Carousel/Carousel';
-import GraphicsSection from '../components/graphicsSection/GraphicsSection';
-import HeroBanner from '../components/hero-banner/HeroBanner';
-import Faq from '../components/faq/Faq';
+import NavBar from "../components/nav-bar/NavBar";
+import Carousel from "../components/Carousel/Carousel";
+import GraphicsSection from "../components/graphicsSection/GraphicsSection";
+import HeroBanner from "../components/hero-banner/HeroBanner";
+import Faq from "../components/faq/Faq";
 
 function HomePage(props) {
   return (
