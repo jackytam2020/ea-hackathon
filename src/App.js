306f22435a94c1b92822a43a6353fcca28b06455
unknown <jackytam2020@gmail.com>
2022-12-02 01:02:45 -0800

    added carousel

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.scss';
+import Carousel from './components/Carousel/Carousel';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Carousel />
+    </div>
+  );
 }
 
 export default App;
